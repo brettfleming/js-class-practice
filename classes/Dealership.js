331cd2bd20a63a7cc7ef6aa81dd398a849d287c3
Brett Fleming <brettfleming015@gmail.com>
2021-09-02 12:37:39 -0500

    dealership class

---
+++ classes/Dealership.js
@@ -0,0 +1,12 @@
+
+
+
+class Dealership {
+    constructor(name, owner, employeeCount) {
+        this.name = name;
+        this.owner = owner;
+        this.employeeCount = employeeCount;
+        this.cars = []
+        this.employees = []
+    }
+}
