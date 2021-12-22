203d6a6f14808aa6ef72d1ddef97c130c23a0260
Brett Fleming <brettfleming015@gmail.com>
2021-12-21 21:31:12 -0600

    randomLower function

--- functionPractice/script.js
+++ functionPractice/script.js
@@ -9,3 +9,17 @@ const uppercaseElement = document.getElementById('uppercase')
 const numbersElement = document.getElementById('numbers')
 const symbolsElement = document.getElementById('symbols')
 const symbols2Element = document.getElementById('symbols2')
+
+
+const randomfunction = {
+    lowercase: randomLower,
+    uppercase: randomUpper,
+    number: randomNumber,
+    symbol: randomSymbol,
+    symbol2: randomSymbol2
+  };
+
+  function randomLower() {
+    const lower = "abcdefghijklmnopqrstuvwxyz"
+    return lower[Math.floor(Math.random() * 26)]
+  }
