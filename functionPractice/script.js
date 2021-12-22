9905b911626b17fb10568e0c5701be0f0f57f116
Brett Fleming <brettfleming015@gmail.com>
2021-12-21 21:29:21 -0600

    setting up the veriables

---
+++ functionPractice/script.js
@@ -0,0 +1,11 @@
+const passwordElement = document.getElementById('password')
+const generateElement = document.getElementById('generate')
+
+
+
+const lengthElement = document.getElementById('length')
+const lowercaseElement = document.getElementById('lowercase')
+const uppercaseElement = document.getElementById('uppercase')
+const numbersElement = document.getElementById('numbers')
+const symbolsElement = document.getElementById('symbols')
+const symbols2Element = document.getElementById('symbols2')
