dede3eedbdbe82bd250cdc1a2719bb6e0c930438
Brett Fleming <brettfleming015@gmail.com>
2021-12-22 21:35:05 -0600

    selecting everything that is needed

--- weather/script.js
+++ weather/script.js
@@ -5,3 +5,11 @@ $("#day2Date").text(today.add(1, "days").format("dddd MMMM Do"));
 $("#day3Date").text(today.add(2, "days").format("dddd MMMM Do"));
 $("#day4Date").text(today.add(3, "days").format("dddd MMMM Do"));
 $("#day5Date").text(today.add(4, "days").format("dddd MMMM Do"));
+
+let searchBtn = document.getElementById('searchBtn');
+let searchedCity = document.getElementById('searchedCity');
+let cityName = document.getElementById("city");
+let todaysTemp = document.getElementById('temperature');
+let todaysWindSpeed = document.getElementById('wind-speed');
+let todayshumidity = document.getElementById('humidity');
+let pastCities = document.getElementById('pastCities');
