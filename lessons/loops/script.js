4039289ba77b2acc714429115231b0bdd9ceddfb
Brett Fleming <brettfleming015@gmail.com>
2021-12-29 16:56:31 -0600

    practicing loops

--- lessons/loops/script.js
+++ lessons/loops/script.js
@@ -1,1 +1,6 @@
 let vacationSpots = ['New York', 'L.A.', 'Boston']
+
+//This is done the long way without loops
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
