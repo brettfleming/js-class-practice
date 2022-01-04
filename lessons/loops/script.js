b467567870c3fa0edbe2e3fa4bb3edc01270a168
Brett Fleming <brettfleming015@gmail.com>
2022-01-03 21:13:10 -0600

    working on do while loops

--- lessons/loops/script.js
+++ lessons/loops/script.js
@@ -53,3 +53,9 @@ while (currentCard !== 'spade') {
 let cupsOfSugarNeeded = 5
 
 let cupsAdded = 0
+
+do {
+  cupsAdded++
+  console.log(cupsAdded + ' cup was added') 
+ } while (cupsAdded < cupsOfSugarNeeded);
+  
