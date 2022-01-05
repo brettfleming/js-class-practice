a8578a47534e2c3dff3bc758527daf6322a5eb56
Brett Fleming <brettfleming015@gmail.com>
2022-01-05 09:38:54 -0600

    adding comments to my code

--- lessons/moreJavaScript/script.js
+++ lessons/moreJavaScript/script.js
@@ -1,4 +1,6 @@
+//higher order functions
 
+//this function has a long discriptive name which is good but it could be terrible to have to repeat
 const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
     for(let i = 1; i <= 1000000; i++) {
       if ( (2 + 2) != 4) {
@@ -6,7 +8,7 @@ const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
       }
     }
   }
-  
+  // we can assign our long discriptive name inside another function varible to make it easier to call multiplue times
   let is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes
   
   is2p2();
