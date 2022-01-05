716b3a80b29f66a8d6e4bc6dfce8f77a677daf4a
Brett Fleming <brettfleming015@gmail.com>
2022-01-04 20:20:23 -0600

    working with for loops

--- lessons/loops/script.js
+++ lessons/loops/script.js
@@ -76,5 +76,13 @@ let vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = []
 
 for (let i = 0; i < input.length; i++) {
-  
+  // console.log('i is '+ i);
+  for (let j = 0; j < vowels.length; j++) {
+    // console.log('j is '+ j);
+    if (input[i] === vowels[j]){
+      resultArray.push(input[i]);
+      console.log(input[i]);
+    }
+  }
 }
+console.log(resultArray)
