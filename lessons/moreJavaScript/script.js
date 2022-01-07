a24bae391041551e38cb5efef7f34461f6edb660
Brett Fleming <brettfleming015@gmail.com>
2022-01-07 09:05:20 -0600

    working with the findindex method

--- lessons/moreJavaScript/script.js
+++ lessons/moreJavaScript/script.js
@@ -100,3 +100,13 @@ const longFavoriteWords = favoriteWords.filter(word => {
   return word.length > 7
 })
  console.log(longFavoriteWords)
+//The findIndex method
+ const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+//finds the index of the element you are looking for
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant'
+})
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's'
+})
