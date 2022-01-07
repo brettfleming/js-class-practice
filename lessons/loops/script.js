e169cdf04239ea46dc66f34180015bed9b1805dc
Brett Fleming <brettfleming015@gmail.com>
2022-01-06 20:52:40 -0600

    adding comments to my code

--- lessons/loops/script.js
+++ lessons/loops/script.js
@@ -77,7 +77,7 @@ for (let i = 0; i < rapperArray.length; i++) {
 console.log("And if you don't know, now you know.")
 
 //this is a whale language translator
-
+//it takes the input phrase or word and changes it into whale 
 let input = 'turpentine and turtles'
 let vowels = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = []
