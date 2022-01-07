9888f5e5727e591f1495231d0127869066189a49
Brett Fleming <brettfleming015@gmail.com>
2022-01-07 09:14:10 -0600

    working with the reduce method

--- lessons/moreJavaScript/script.js
+++ lessons/moreJavaScript/script.js
@@ -110,3 +110,15 @@ const foundAnimal = animals.findIndex(animal => {
 const startsWithS = animals.findIndex(animal => {
   return animal[0] === 's'
 })
+
+//the reduce Method
+const newNumbers = [1, 3, 5, 7];
+
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue
+}, 10)
+
+console.log(newSum)
