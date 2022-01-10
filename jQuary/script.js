ff22ae4ee1facc9c3f53327c3e9c38b289c6256b
Brett Fleming <brettfleming015@gmail.com>
2022-01-09 20:55:02 -0600

    adding comments to may code

--- jQuary/script.js
+++ jQuary/script.js
@@ -5,6 +5,7 @@ const day = [
     {hour: "9Am", hourNumber: 9}, {hour: "10Am", hourNumber: 10}, {hour: "11Am", hourNumber: 11}, {hour: "12Am", hourNumber: 12}, {hour: "1pm", hourNumber: 13},
      {hour: "2pm", hourNumber: 14},  {hour: "3pm", hourNumber: 15}, {hour: "4pm", hourNumber: 16}, {hour: "5pm", hourNumber: 17}
 ]
+//list of hours of the day
 const timeOfDay = [
     "9",
     "10",
