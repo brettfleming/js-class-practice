2b33477b6b2c9b00cce18341d94183a4f471aefa
Brett Fleming <brettfleming015@gmail.com>
2022-01-10 20:51:51 -0600

    adding comments

--- lessons/objects/script.js
+++ lessons/objects/script.js
@@ -5,7 +5,7 @@ let fasterShip = {
   numCrew: 5,
   flightPath: ['Venus', 'Mars', 'Saturn']
   }
-
+//dot notation is how you select properties of an object so they can be used
 let crewCount = spaceship.numCrew;
 
 let planetArray = spaceship.flightPath;
