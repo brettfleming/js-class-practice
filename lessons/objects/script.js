75fbf68299520c3f21f7cec47f0601fcfcb1afa3
Brett Fleming <brettfleming015@gmail.com>
2022-01-12 20:34:08 -0600

    working on nested objects

--- lessons/objects/script.js
+++ lessons/objects/script.js
@@ -45,3 +45,32 @@ const alienShip = {
 alienShip.retreat();
 
 alienShip.takeOff();
+//nested objects
+let spaceship3 = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
