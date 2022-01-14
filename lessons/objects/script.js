3469a2d6bcca7a0c1dd76499ae93239123d59423
Brett Fleming <brettfleming015@gmail.com>
2022-01-13 21:00:16 -0600

    adding comments

--- lessons/objects/script.js
+++ lessons/objects/script.js
@@ -1,3 +1,19 @@
+//objects
+
+
+//Key Points
+  //Objects store collections of key-value pairs.
+  //Each key-value pair is a property—when a property is a function it is known as a method.
+  //An object literal is composed of comma-separated key-value pairs surrounded by curly braces.
+  //You can access, add or edit a property within an object by using dot notation or bracket notation.
+  //We can add methods to our object literals using key-value syntax with anonymous function expressions as values or by using the new ES6 method syntax.
+  //We can navigate complex, nested objects by chaining operators.
+  //Objects are mutable—we can change their properties even when they’re declared with const.
+  //Objects are passed by reference— when we make changes to an object passed into a function, those changes are permanent.
+  //We can iterate through objects using the For...in syntax
+
+
+
 let fasterShip = {
     homePlanet: 'Earth',
   color: 'silver',
@@ -129,6 +145,6 @@ for (let crewMember in spaceship5.crew) {
 console.log(`${crewMember}: ${spaceship5.crew[crewMember].name}`);
 }
 //this for in loop iterates through each crewmember and their degree
-for (let crewMember in spaceship.crew) {
-  console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`);
+for (let crewMember in spaceship5.crew) {
+  console.log(`${spaceship5.crew[crewMember].name}: ${spaceship5.crew[crewMember].degree}`);
 }
