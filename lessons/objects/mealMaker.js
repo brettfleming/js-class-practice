da427b714c52d42a66bdc1b4d75fc8cb8e364ad3
Brett Fleming <brettfleming015@gmail.com>
2022-01-14 11:00:29 -0600

    starting the meal maker project

---
+++ lessons/objects/mealMaker.js
@@ -0,0 +1,26 @@
+const menu = {
+    _courses = {
+        appetizers: [],
+        mains: [],
+        desserts: [],
+    },
+    get appetizers() {
+  
+    }, 
+    set appetizers(appetizer) {
+  
+    },
+    get mains() {
+  
+    }, 
+    set mains(main) {
+  
+    },
+    get desserts() {
+  
+    }, 
+    set desserts(dessert) {
+  
+    },
+  
+  };
