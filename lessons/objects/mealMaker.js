2e917e54d361634f89c6a82fcaf827c343732fdb
Brett Fleming <brettfleming015@gmail.com>
2022-01-14 11:57:46 -0600

    adding comments

--- lessons/objects/mealMaker.js
+++ lessons/objects/mealMaker.js
@@ -5,64 +5,64 @@ const menu = {
         desserts: [],
     },
     get appetizers() {
-  
-    }, 
+
+    },
     set appetizers(appetizer) {
-  
+
     },
     get mains() {
-  
-    }, 
+
+    },
     set mains(main) {
-  
+
     },
     get desserts() {
-  
-    }, 
+
+    },
     set desserts(dessert) {
-  
+
     },
     get courses() {
-      return {
-        appetizers: this.appetizers,
-        mains: this.mains,
-        desserts: this.desserts
-      }
+        return {
+            appetizers: this.appetizers,
+            mains: this.mains,
+            desserts: this.desserts
+        }
     },
     addDishToCourse(courseName, dishName, dishPrice) {
-      const dish = {
-        name: dishName,
-        price: dishPrice,
-      };
-      this._courses[courseName].push(dish);
+        const dish = {
+            name: dishName,
+            price: dishPrice,
+        };
+        this._courses[courseName].push(dish);
     },
     getRandomDishFromCourse(courseName) {
-      const dishes = this._courses[courseName]
-      const randomIndex = Math.floor(Math.random() * dishes.length);
-      return dishes[randomIndex]
-  
+        const dishes = this._courses[courseName]
+        const randomIndex = Math.floor(Math.random() * dishes.length);
+        return dishes[randomIndex]
+
     },
     generateRandomMeal() {
-      const appetizer = this.getRandomDishFromCourse('appetizers');
-      const main = this.getRandomDishFromCourse('mains');
-      const dessert = this.getRandomDishFromCourse('desserts');
-      console.log(`The appetizer is ${appetizer.name}, the main course is ${main.name}, finally dessert will be ${dessert.name}, total price: ${appetizer.price + main.price + dessert.price} `)
-  
+        const appetizer = this.getRandomDishFromCourse('appetizers');
+        const main = this.getRandomDishFromCourse('mains');
+        const dessert = this.getRandomDishFromCourse('desserts');
+        console.log(`The appetizer is ${appetizer.name}, the main course is ${main.name}, finally dessert will be ${dessert.name}, total price: ${appetizer.price + main.price + dessert.price} `)
+
     }
-  
-  };
-  
-  menu.addDishToCourse('appetizers', 'chicken wings', 8);
-  menu.addDishToCourse('appetizers', 'chicken tenders', 5);
-  menu.addDishToCourse('appetizers', 'onion rings', 6);
-  
-  menu.addDishToCourse('mains', 'chicken alfrado', 23);
-  menu.addDishToCourse('mains', 'Ribeye steak', 30);
-  menu.addDishToCourse('mains', 'chicken cesear salad', 15);
-  
-  menu.addDishToCourse('desserts', 'chacolate cake', 10);
-  menu.addDishToCourse('desserts', 'cookie and ice cream', 7);
-  menu.addDishToCourse('desserts', 'strawberry cheesecake', 12);
-  
-  menu.generateRandomMeal()
-  
+
+};
+
+//adding dishes to the appetizers array
+menu.addDishToCourse('appetizers', 'chicken wings', 8);
+menu.addDishToCourse('appetizers', 'chicken tenders', 5);
+menu.addDishToCourse('appetizers', 'onion rings', 6);
+//adding dishes to the mains array
+menu.addDishToCourse('mains', 'chicken alfrado', 23);
+menu.addDishToCourse('mains', 'Ribeye steak', 30);
+menu.addDishToCourse('mains', 'chicken cesear salad', 15);
+//adding dishes to the desserts array
+menu.addDishToCourse('desserts', 'chacolate cake', 10);
+menu.addDishToCourse('desserts', 'cookie and ice cream', 7);
+menu.addDishToCourse('desserts', 'strawberry cheesecake', 12);
+
+menu.generateRandomMeal()
