a4d84c47c1a83b233e8615ea716596631dd7aeb7
Brett Fleming <brettfleming015@gmail.com>
2022-01-14 11:49:09 -0600

    added the generate Random meal method to the menu object

--- lessons/objects/mealMaker.js
+++ lessons/objects/mealMaker.js
@@ -35,6 +35,19 @@ const menu = {
         price: dishPrice,
       };
       this._courses[courseName].push(dish);
+    },
+    getRandomDishFromCourse(courseName) {
+      const dishes = this._courses[courseName]
+      const randomIndex = Math.floor(Math.random() * dishes.length);
+      return dishes[randomIndex]
+  
+    },
+    generateRandomMeal() {
+      const appetizer = getRandomDishFromCourse(appetizers);
+      const main = getRandomDishFromCourse(mains);
+      const dessert = getRandomDishFromCourse(desserts);
+      console.log(`The appetizer is ${appetizer.name}, the main course is ${main.name}, finally dessert will be ${dessert.name}, total price: ${appetizer.price + main.price + dessert.price} `)
+  
     }
   
   };
