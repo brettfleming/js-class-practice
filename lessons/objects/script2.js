9b30a0aa52dfb96e4fbdb1e8ddf9a880730124e3
Brett Fleming <brettfleming015@gmail.com>
2022-01-14 09:58:29 -0600

    working with getter methods

--- lessons/objects/script2.js
+++ lessons/objects/script2.js
@@ -1,3 +1,5 @@
+//advanced object stuff
+
 const robot = {
     model: '1E78V2',
     energyLevel: 100,
@@ -8,3 +10,20 @@ const robot = {
 
 // console.log's, I am 1E78V2 and my current energy level is 100   
 console.log(robot.provideInfo())
+
+
+//getter methods
+const robot2 = {
+    _model: '1E78V2',
+    _energyLevel: 100,
+    get energyLevel() {
+      if (typeof this._energyLevel === 'number') {
+        return `My current energy level is ${this._energyLevel}`
+      } else {
+        return `System malfunction: cannot retrieve energy level`
+      }
+    }
+  };
+  
+  
+  console.log(robot2.energyLevel)
