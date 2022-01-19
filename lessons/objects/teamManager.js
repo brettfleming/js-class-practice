194699f7c46a322a12c202d9bf39226bc062f797
Brett Fleming <brettfleming015@gmail.com>
2022-01-18 20:42:00 -0600

    added a comment

--- lessons/objects/teamManager.js
+++ lessons/objects/teamManager.js
@@ -1,47 +1,48 @@
 const team = {
     _players: [
-      {
-    firstName: 'James',
-    lastName: 'Johnson',
-    age: 21
-  },
-  {
-    firstName: 'Paul',
-    lastName: 'Peterson',
-    age: 28
-  },
-  {
-    firstName: 'Timmy',
-    lastName: 'Turner',
-    age: 18
-  }
+        {
+            firstName: 'James',
+            lastName: 'Johnson',
+            age: 21
+        },
+        {
+            firstName: 'Paul',
+            lastName: 'Peterson',
+            age: 28
+        },
+        {
+            firstName: 'Timmy',
+            lastName: 'Turner',
+            age: 18
+        }
     ],
-    _games:[
-      {
-    opponent: 'Birds',
-    teamPoints: 55,
-    opponentPoints: 15
-  },
-  {
-    opponent: 'Monkeys',
-    teamPoints: 62,
-    opponentPoints: 30
-  },
-  {
-    opponent: 'Tigers',
-    teamPoints: 40,
-    opponentPoints: 39
-  }
+    _games: [
+        {
+            opponent: 'Birds',
+            teamPoints: 55,
+            opponentPoints: 15
+        },
+        {
+            opponent: 'Monkeys',
+            teamPoints: 62,
+            opponentPoints: 30
+        },
+        {
+            opponent: 'Tigers',
+            teamPoints: 40,
+            opponentPoints: 39
+        }
     ],
     get games() {
-         return this._games;
-     },
-     addPlayer(firstName, LastName, age) {
-       let player = {
-         firstName,
-         lastName,
-         age
-       };
-       this.player.push(player);
-     }
-  }
+        return this._games;
+    },
+    //this method uses destructed assignment to assign the values to the player object
+    addPlayer(firstName, lastName, age) {
+        let player = {
+            firstName,
+            lastName,
+            age
+        };
+        this.player.push(player);
+    }
+}
