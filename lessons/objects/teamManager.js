c0f8b5766633ba31cb1fb3a593607f7c5bf1a329
Brett Fleming <brettfleming015@gmail.com>
2022-01-18 20:34:22 -0600

    added players array with starting data of 3 players, and an empty games array

--- lessons/objects/teamManager.js
+++ lessons/objects/teamManager.js
@@ -1,3 +1,20 @@
 const team = {
-  
-}
+    _players: [
+      {
+    firstName: 'James',
+    lastName: 'Johnson',
+    age: 21
+  },
+  {
+    firstName: 'Paul',
+    lastName: 'Peterson',
+    age: 28
+  },
+  {
+    firstName: 'Timmy',
+    lastName: 'Turner',
+    age: 18
+  }
+    ],
+    _games:[]
+  }
