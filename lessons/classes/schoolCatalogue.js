4b80914e44ee840c3b5b93bb519c6d52df2cf198
Brett Fleming <brettfleming015@gmail.com>
2022-01-28 10:06:37 -0600

    added the school class

---
+++ lessons/classes/schoolCatalogue.js
@@ -0,0 +1,20 @@
+class School {
+    constructor(name, numberOfStudents) {
+      this._name = name;
+      this._numberOfStudents = numberOfStudents
+    } 
+  
+    get name() {
+      return this._name
+    }
+    get numberOfStudents() {
+      return this._numberOfStudents
+    }
+  
+    quickFacts() {
+  
+    }
+    static pickSubstituteTheacher() {
+  
+    }
+  }
