4a7e62081a6144672b94de6fe302729986582b6c
Brett Fleming <brettfleming015@gmail.com>
2022-01-28 09:45:26 -0600

    added the movie subclass

--- lessons/classes/library.js
+++ lessons/classes/library.js
@@ -49,3 +49,20 @@ class Media {
       return this._pages
     }
   }
+
+
+  class Movie extends Media {
+    constructor(title, director, runTime){
+      super(title)
+      this._director = director;
+      this._runTime = runTime;
+    }
+  
+    get director() {
+      return this._director;
+    }
+  
+    get runTime() {
+      return this._runTime;
+    }
+  }
