f5d42db40d6d60b144a0adaac3c79dbf27f09b59
Brett Fleming <brettfleming015@gmail.com>
2022-01-27 20:41:57 -0600

    added some comments

--- lessons/classes/script.js
+++ lessons/classes/script.js
@@ -1,5 +1,14 @@
 // intro to classes
 
+// key points
+
+    //Classes are templates for objects.
+    //Javascript calls a constructor method when we create a new instance of a class.
+    //Inheritance is when we create a parent class with properties and methods that we can extend to child classes.
+    //We use the extends keyword to create a subclass.
+    //The super keyword calls the constructor() of a parent class.
+    //Static methods are called on the class, but not on instances of the class.
+
 //classes are special types of objects that are like templates to mass produce objects
 class Dog {
     //the constructor is how you can create a new object 
