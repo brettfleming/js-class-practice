27f739fc9ec4f1b4a20a4eb38a23de3e51288180
Brett Fleming <brettfleming015@gmail.com>
2022-01-28 10:46:38 -0600

    checking my work

--- lessons/classes/schoolCatalogue.js
+++ lessons/classes/schoolCatalogue.js
@@ -34,7 +34,7 @@ class School {
 }
 
 class Primary extends School {
-    constructor(name, level, numberOfStudents, pickupPolicy) {
+    constructor(name, numberOfStudents, pickupPolicy) {
         super(name, 'primary', numberOfStudents)
         this._pickupPolicy = pickupPolicy
     }
@@ -45,13 +45,13 @@ class Primary extends School {
 }
 
 class Middle extends School {
-    constructor(name, level, numberOfStudents) {
+    constructor(name, numberOfStudents) {
         super(name, 'middle', numberOfStudents)
     }
 }
 
 class High extends School {
-    constructor(name, level, numberOfStudents, sportsTeams) {
+    constructor(name, numberOfStudents, sportsTeams) {
         super(name, 'high', numberOfStudents)
         this._sportsTeams = sportsTeams
     }
@@ -60,3 +60,10 @@ class High extends School {
         return this._sportsTeams
     }
 }
+
+const lorraineHansbury = new Primary('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.')
+lorraineHansbury.quickFacts()
+const sub = School.pickSubstituteTheacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli'])
+const alSmith = new High('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field'])
+
+console.log(alSmith.sportsTeams)
