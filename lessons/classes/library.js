fee18e7ff2a708160b3a5bf455ec22b6cc5cdaca
Brett Fleming <brettfleming015@gmail.com>
2022-01-28 09:34:14 -0600

    finished parent class

--- lessons/classes/library.js
+++ lessons/classes/library.js
@@ -7,19 +7,29 @@ class Media {
     get title(){
       return this._title
     }
-    get isCheckOut(){
+    get isCheckedOut(){
       return this._isCheckedOut;
     }
     get ratings() {
       return this._ratings
     }
     getAverageRating() {
+      const average = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
+  
+      return average(this._ratings).toFixed(1)
+      
   
     }
     toggleCheckOutStatus() {
+        if(this._isCheckedOut === false) {
+          this._isCheckedOut = true
+        } else {
+          this._isCheckedOut = false
+        }
   
     }
     addRating(rating) {
-      
+      this._ratings.push(rating)
+  
     }
   }
