3f7c15a11ee66653f555b743f18075de610d9313
Brett Fleming <brettfleming015@gmail.com>
2022-01-28 10:36:16 -0600

    finsihed up the classes and added lines to the methods

--- lessons/classes/schoolCatalogue.js
+++ lessons/classes/schoolCatalogue.js
@@ -1,61 +1,62 @@
 class School {
     constructor(name, level, numberOfStudents) {
-      this._name = name;
-      this._level = level
-      this._numberOfStudents = numberOfStudents
-    } 
-  
+        this._name = name;
+        this._level = level
+        this._numberOfStudents = numberOfStudents
+    }
+
     get name() {
-      return this._name
+        return this._name
     }
     get level() {
-      return this._level
+        return this._level
     }
     get numberOfStudents() {
-      return this._numberOfStudents
+        return this._numberOfStudents
     }
 
     set numberOfStudents(number) {
-        if (typeof number === 'number' ) {
-          this._numberOfStudents = number
+        if (typeof number === 'number') {
+            this._numberOfStudents = number
         } else {
-          console.log('Invalid input: numberOfStudents must be set to a Number.')
+            console.log('Invalid input: numberOfStudents must be set to a Number.')
         }
-      }
-  
-    quickFacts() {
-  
     }
-    static pickSubstituteTheacher() {
-  
-    }
-  }
-  
-  class Primary extends School {
-    constructor(name, level, numberOfStudents ) {
-    super(name, level, numberOfStudents)
-    this._pickupPolicy = ''
-    }
-  
-    get pickupPolicy() {
-      return this._pickupPolicy
-    }
-  }
-  
-  class Middle extends School {
-    constructor(name, level, numberOfStudents ) {
-    super(name, level, numberOfStudents)
-    }
-  }
 
-  class High extends School {
-    constructor(name, level, numberOfStudents, sportsTeams  ) {
-    super(name, level, numberOfStudents)
-    this._sportsTeams = sportsTeams
+    quickFacts() {
+        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`)
+
     }
-  
+    static pickSubstituteTheacher(substituteTeachers) {
+        return substituteTeachers[Math.floor(Math.random() * substituteTeachers.length)]
+
+    }
+}
+
+class Primary extends School {
+    constructor(name, level, numberOfStudents, pickupPolicy) {
+        super(name, 'primary', numberOfStudents)
+        this._pickupPolicy = pickupPolicy
+    }
+
+    get pickupPolicy() {
+        return this._pickupPolicy
+    }
+}
+
+class Middle extends School {
+    constructor(name, level, numberOfStudents) {
+        super(name, 'middle', numberOfStudents)
+    }
+}
+
+class High extends School {
+    constructor(name, level, numberOfStudents, sportsTeams) {
+        super(name, 'high', numberOfStudents)
+        this._sportsTeams = sportsTeams
+    }
+
     get sportsTeams() {
-      return this._sportsTeams 
+        return this._sportsTeams
     }
-  }
-  
+}
