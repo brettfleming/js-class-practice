f732dfc0d25f49c37313a181346ab6f15cfe0722
Brett Fleming <brettfleming015@gmail.com>
2022-01-27 19:38:04 -0600

    working with classes

--- lessons/classes/script.js
+++ lessons/classes/script.js
@@ -26,3 +26,12 @@ console.log(halley.behavior); // Print behavior value to console
 halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 console.log(halley.behavior); // Print behavior value to console
+
+
+class Surgeon {
+    constructor(name, department) {
+      this.name = name
+      this.department = department
+    }
+  
+  }
