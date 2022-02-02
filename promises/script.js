9c9ac202982a0020b5a8fda16dc9e59b9b2bd87b
Brett Fleming <brettfleming015@gmail.com>
2022-02-02 09:19:55 -0600

    set up library.js

--- promises/script.js
+++ promises/script.js
@@ -38,3 +38,9 @@ setTimeout(usingSTO, 2000);
 
 //this will console log before usingSTO will excicute
 console.log("This is the last line of code in app.js.");
+
+
+
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
