9f0605fd7698b834e20cf156b2862bd43add1573
Brett Fleming <brettfleming015@gmail.com>
2022-02-02 09:22:00 -0600

    added failure handler

--- promises/script.js
+++ promises/script.js
@@ -48,3 +48,7 @@ const order = [['sunglasses', 1], ['bags', 2]];
 const handleSuccess = (resolvedValue) => {
     console.log(resolvedValue);
   };
+
+  const handleFailure = (rejectionReason) => {
+    console.log(rejectionReason);
+  };
