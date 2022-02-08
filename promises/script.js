42c9c7e0828eaf5a5400e9c6413b4d5244e4fb30
Brett Fleming <brettfleming015@gmail.com>
2022-02-08 09:54:02 -0600

    practicing refactoring bad code

--- promises/script.js
+++ promises/script.js
@@ -99,3 +99,23 @@ checkInventory(order)
 .catch((errorMessage) => {
   console.log(errorMessage);
 });
+
+
+//refactoring bad code
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+// checkInventory(order)
+//     .then((resolvedValueArray) => {
+//         processPayment(resolvedValueArray)
+//             .then((resolvedValueArray) => {
+//                 shipOrder(resolvedValueArray)
+//                     .then((successMessage) => {
+//                         console.log(successMessage);
+//                     });
+//             });
+//     });
