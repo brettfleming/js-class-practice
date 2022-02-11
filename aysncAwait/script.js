74e86a53e7eadde48ac9f38c2690679a1529fefb
Brett Fleming <brettfleming015@gmail.com>
2022-02-11 12:39:43 -0600

    added the async await example

--- aysncAwait/script.js
+++ aysncAwait/script.js
@@ -24,3 +24,12 @@ promisifiedReadfile('./file.txt', 'utf-8')
     console.log(firstSentence, secondSentence)
   })
   .catch((err) => {console.log(err)});
+
+  // Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
+async function readFiles() {
+    let firstSentence = await promisifiedReadfile('./file.txt', 'utf-8');
+    let secondSentence = await promisifiedReadfile('./file2.txt', 'utf-8');
+    console.log(firstSentence, secondSentence);
+  }
+  
+  readFiles();
