c40a657cfab59f86fe1246ea989eb30579b45d15
Brett Fleming <brettfleming015@gmail.com>
2022-02-14 20:25:18 -0600

    added in the async/await version of the function

--- aysncAwait/script2.js
+++ aysncAwait/script2.js
@@ -39,3 +39,10 @@ function nativePromiseDinner() {
 	  console.log(`I'm going to make ${meal} for dinner.`);
   });
 }
+
+// async/await version:
+async function announceDinner() {
+  let meal = await brainstormDinner();
+  console.log(`I'm going to make ${meal} for dinner.`)
+}
+announceDinner();
