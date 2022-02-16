ad1c32a6b73e7cd11fc2705a234ca18f6c23cb8f
Brett Fleming <brettfleming015@gmail.com>
2022-02-15 20:41:47 -0600

    adding the getbeans function

--- aysncAwait/script.js
+++ aysncAwait/script.js
@@ -33,3 +33,13 @@ async function readFiles() {
   }
   
   readFiles();
+
+  const shopForBeans = require('./library.js');
+
+ function getBeans() {
+  console.log(`1. Heading to the store to buy beans...`);
+  let value = shopForBeans();
+  console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
+}
+
+getBeans();
