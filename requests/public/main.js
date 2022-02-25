7292171397ad99931defde5e2215bb8bd988966a
Brett Fleming <brettfleming015@gmail.com>
2022-02-24 20:49:23 -0600

    adding in the generateJSon function

--- requests/public/main.js
+++ requests/public/main.js
@@ -2,3 +2,17 @@ const jsonButton = document.querySelector('#generate');
 const buttonContainer = document.querySelector('#buttonContainer');
 const display = document.querySelector('#displayContainer');
 const collection = ["Another", "More", "Next", "Continue", "Keep going", "Click me", "A new one"];
+
+const generateJson = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.onReadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
+                changeButton();
+        }
+    }
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+}
