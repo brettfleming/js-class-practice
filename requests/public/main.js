a785e51df0db8c26a358883c7332f9ef2b57fffa
Brett Fleming <brettfleming015@gmail.com>
2022-02-24 20:50:25 -0600

    adding in the formatJSon function

--- requests/public/main.js
+++ requests/public/main.js
@@ -6,13 +6,34 @@ const collection = ["Another", "More", "Next", "Continue", "Keep going", "Click 
 const generateJson = () => {
     const xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-
-    xhr.onReadystatechange = () => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            renderResponse(xhr.response);
-                changeButton();
-        }
+    
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+    renderResponse(xhr.response);
+        changeButton();
+      }
     }
     xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
     xhr.send();
-}
+  }
+
+const formatJson = (resJson) => {
+    resJson = JSON.stringify(resJson);
+    let counter = 0;
+    return resJson.split('')
+    .map(char => {
+      switch (char) {
+        case ',':
+          return `,\n${' '.repeat(counter * 2)}`;
+        case '{':
+          counter += 1;
+          return `{\n${' '.repeat(counter * 2)}`;
+        case '}':
+          counter -= 1;
+          return `\n${' '.repeat(counter * 2)}}`;
+        default:
+          return char;
+      }
+    })
+    .join('');
+  }
