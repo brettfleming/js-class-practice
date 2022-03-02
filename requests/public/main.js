1d62067cecf4396b98d1e4dce81e47c79be93ccd
Brett Fleming <brettfleming015@gmail.com>
2022-03-01 21:11:21 -0600

    finsihed the boiler plate AJAX response

--- requests/public/main.js
+++ requests/public/main.js
@@ -71,6 +71,9 @@ const formatJson = (resJson) => {
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
- 
+      return xhr.response
     }
   }
+
+  xhr.open('GET', url);
+xhr.send();
