b86ce36eefc159a77df5cd6e5d1d4e0382fc5677
Brett Fleming <brettfleming015@gmail.com>
2022-03-07 20:43:45 -0600

    starting the next request exercise

--- requests/public/main.js
+++ requests/public/main.js
@@ -77,3 +77,11 @@ const formatJson = (resJson) => {
 
   xhr.open('GET', url);
 xhr.send();
+
+// Information to reach API
+const url = 'https://api.datamuse.com/words?'
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
