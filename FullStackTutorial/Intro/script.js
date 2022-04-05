3ac3e1e840b39841987d29caf25ed00e6a9bf149
Brett Fleming <brettfleming015@gmail.com>
2022-04-04 19:59:27 -0500

    adding comments

--- FullStackTutorial/Intro/script.js
+++ FullStackTutorial/Intro/script.js
@@ -1,6 +1,9 @@
+//varihbles are the most basic peice of code you can set them equal to things
 let name = 'smith';
 let age = 23;
 
+//we can use varibles to insert information into other varibles
 let message = "Hi, my name is " + name + " and I am " + age + " years old!";
 
+//they can also be used to hold a message that is ment to be displayed
 alert(message);
