5a88532edd32eab76b26bccd440ad867e4644c92
Brett Fleming <brettfleming015@gmail.com>
2022-04-04 20:18:38 -0500

    working with varibles

---
+++ FullStackTutorial/variables/script.js
@@ -0,0 +1,13 @@
+
+//varibles are stored values in memory
+//key words used to declare a varible are let and const
+//the type of this variable is a string
+let name = 'tim';
+//the type of this variable is a number
+//let is used when the varible can be redeclared this isnt used very often
+let age = 23;
+
+
+//const is used when the varibnle is not meant to be redclared 
+const lastName = 'smith'
+
