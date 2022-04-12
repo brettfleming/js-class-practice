5720ac6904f09c44b1e058b9d194c3ef681c6393
Brett Fleming <brettfleming015@gmail.com>
2022-04-11 20:55:44 -0500

    reviewing aysncAwait

--- aysncAwait/script2.js
+++ aysncAwait/script2.js
@@ -123,3 +123,18 @@ try {
 } catch (e) {
   alert( "Invalid JSON data!" );
 }
+
+function helloWorld() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('Hello World!');
+    }, 2000);
+  });
+}
+ 
+async function msg() {
+  const msg = await helloWorld();
+  console.log('Message:', msg);
+}
+ 
+msg(); // Message: Hello World! <-- after 2 seconds
