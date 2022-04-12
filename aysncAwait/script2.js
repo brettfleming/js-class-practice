e7c26a74c110b98631fab81f76455604f8d187f3
Brett Fleming <brettfleming015@gmail.com>
2022-04-11 20:55:21 -0500

    reviewing aysncAwait

--- aysncAwait/script2.js
+++ aysncAwait/script2.js
@@ -115,3 +115,11 @@ async function serveDinnerAgain() {
 
 serveDinnerAgain();
 
+let json = '{ "age": 30 }'; // incomplete data
+ 
+try {
+  let user = JSON.parse(json); // <-- no errors
+  alert( user.name ); // no name!
+} catch (e) {
+  alert( "Invalid JSON data!" );
+}
