5857719d1db05e9ba6e1f7479c1cf0102b992d7a
Brett Fleming <brettfleming015@gmail.com>
2022-04-11 20:55:57 -0500

    reviewing aysncAwait

--- aysncAwait/script2.js
+++ aysncAwait/script2.js
@@ -138,3 +138,14 @@ async function msg() {
 }
  
 msg(); // Message: Hello World! <-- after 2 seconds
+
+let promise1 = Promise.resolve(5);
+let promise2 = 44;
+let promise3 = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 100, 'foo');
+});
+ 
+Promise.all([promise1, promise2, promise3]).then(function(values) {
+  console.log(values);
+});
+// expected output: Array [5, 44, "foo"]
